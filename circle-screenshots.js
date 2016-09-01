17ea1e20ee27910892e44fb8ff621317266b8624
arypurnomoz <arypurnomoz@gmail.com>
2016-09-02 01:17:22 +0700

    update

--- circle-screenshots.js
+++ circle-screenshots.js
@@ -1,1 +1,6 @@
-console.log('shots', screenshots)
+screenshots.split(' ').forEach(x=> {
+  const dom = document.createElement('div')
+  dom.innerHTML = '<div style="margin-bottom:32px"><div style="margin-bottom:6px">' + x + '</div><img src="./screenshot/' + x + '"/></div>'
+  document.body.appendChild(dom)
+})
+
